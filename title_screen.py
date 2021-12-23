de4a9991c97ecf8e7b167ee7d83bf08eb571b818
SimoneGhosh <91547699+SimoneGhosh@users.noreply.github.com>
2021-12-22 20:04:43 -0500

    Add files via upload

---
+++ title_screen.py
@@ -0,0 +1,18 @@
+import pygame
+from sys import exit
+
+pygame.init()
+screen = pygame.display.set_mode((600,600))
+pygame.display.set_caption("To be named later")
+clock = pygame.time.Clock()
+colour = (0,0,255)
+screen.fill(colour)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    
+    pygame.display.update()
+    clock.tick(60)
