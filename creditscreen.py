0548ebafdb5fc01907fab588f258a5b6b48d6a40
JiangMona <91548088+JiangMona@users.noreply.github.com>
2021-12-24 12:13:12 -0500

    Add files via upload

---
+++ creditscreen.py
@@ -0,0 +1,52 @@
+'''
+author: Mona and Simone
+Date: December 23, 2021
+Name: Credits
+Description: Roll credits before quit
+'''
+
+#import modules
+import pygame
+from pygame.locals import *  
+from pygame.color import THECOLORS   #can use colour names instead of hexidecimal
+import os, time
+# initial library itself
+pygame.init()  
+
+
+#to work on tdsb computer 
+import platform
+if platform.system() == "Windows":
+    os.environ['SDL_VIDEODRIVER'] = 'windib'
+
+#Set-up the main screen display window and caption  in the 
+size = (850,600)  
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("poisonous pyramids")
+surface = pygame.image.load("creditssurface.jpeg")
+screen.blit(surface,(0,0))
+
+
+clock = pygame.time.Clock() #clock
+keepGoing = True 	     
+myfont = pygame.font.SysFont("Papyrus", 50)     #using different font sizes
+mysmallfont = pygame.font.SysFont("Papyrus", 25)
+test=pygame.display.get_driver()
+try:
+    while keepGoing:
+        clock.tick(60) #<-- Set a constant frame rate, argument is frames per second  
+        text=myfont.render("Credits", True, THECOLORS["black"])
+        screen.blit(text,(370,50))
+        credits1=mysmallfont.render("book", True, THECOLORS["black"])
+        screen.blit(credits1,(390,125))
+
+           # Place (or blit) text on certain location (x, y) of the screen
+        pygame.display.flip()
+        #Handle any events in the current frame
+        for ev in pygame.event.get(): 
+            if ev.type == pygame.QUIT: #<-- this special event type happens when the window is closed
+                keepGoing = False
+finally:
+    pygame.quit()  # Keep this IDLE friendly
+    
+
